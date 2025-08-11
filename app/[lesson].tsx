8f83b74a3d3ab5b8e9cc1222b0503c3a28e1d9e3
Justin Haddock <jhadddock@protonmail.com>
2025-08-11 16:57:05 -0700

    Add and edit lesssons for parts of speech and plural nouns

--- app/[lesson].tsx
+++ app/[lesson].tsx
@@ -16,6 +16,7 @@ import articlesSections from '@/i18n/locales/en-us/sections/articles.json';
 import adverbsOfFrequencySections from '@/i18n/locales/en-us/sections/adverbs of frequency.json';
 import illnessesSections from '@/i18n/locales/en-us/sections/illnesses.json';
 import pluralNounsSections from '@/i18n/locales/en-us/sections/plural nouns.json';
+import partsOfSpeech1Sections from '@/i18n/locales/en-us/sections/parts of speech 1.json';
 
 const sectionsData = [
   ...yesOrNoQuestionsSections,
@@ -32,6 +33,7 @@ const sectionsData = [
   ...adverbsOfFrequencySections,
   ...illnessesSections,
   ...pluralNounsSections,
+  ...partsOfSpeech1Sections,
 ];
 import vocabularyData from '@/i18n/locales/en-us/vocabulary.json';
 import VocabCard from '@/components/VocabCard';
